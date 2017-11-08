a5b2be69b88e9487b2112b9b17042a017277d658
Shane <stbarratt@gmail.com>
2017-11-08 15:28:31 -0800

    Wrapped images in a torch.utils.data.DataLoader for simplicity

--- inception_score.py
+++ inception_score.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from torch.autograd import Variable
 from torch.nn import functional as F
+import torch.utils.data
 
 from torchvision.models.inception import inception_v3
 
@@ -15,11 +16,16 @@ def inception_score(imgs, cuda=True, batch_size=32, resize=False):
     cuda -- whether or not to run on GPU
     batch_size -- batch size to feed into inception
     """
-    assert batch_size > 1
+    N = len(imgs)
+
+    assert batch_size > 0
+    assert N > batch_size
 
     if cuda:
-        dtype = torch.cuda.FloatTensor # Uncomment this to run on GPU
+        dtype = torch.cuda.FloatTensor
     else:
+        if torch.cuda.is_available():
+            print("WARNING: You have a CUDA device, so you should probably set cuda=True")
         dtype = torch.FloatTensor
 
     # Load inception model
@@ -33,24 +39,30 @@ def inception_score(imgs, cuda=True, batch_size=32, resize=False):
         return F.softmax(x)
 
     # Get predictions
-    N = len(imgs)
-    imgs = [im.transpose(-1, 0, 1)[None, :] for im in imgs]
     preds = np.zeros((N, 1000))
 
-    for i in range(N // batch_size + 1):
-        start_batch = i*batch_size
-        end_batch = (i+1)*batch_size
-        if end_batch > N:
-            end_batch = N
+    # Wrap imgs in a dataset for simplicity
+    imgs = [im.transpose(-1, 0, 1) for im in imgs]
+    class ImageDataset(torch.utils.data.Dataset):
+        def __init__(self):
+            self.imgs = imgs
 
-        imgs_batch = np.concatenate(imgs[start_batch:end_batch])
-        imgs_batch = torch.Tensor(imgs_batch.astype(np.float32)).type(dtype)
-        imgs_batch = Variable(imgs_batch)
+        def __getitem__(self, index):
+            return imgs[index]
 
-        p_out = get_pred(imgs_batch)
-        preds[start_batch:end_batch, :] = p_out.data.cpu().numpy()
+        def __len__(self):
+            return len(imgs)
+    dataloader = torch.utils.data.DataLoader(ImageDataset(), batch_size=batch_size)
 
-    # Now compute the entropy
+    for i, batch in enumerate(dataloader, 0):
+        batch = batch.type(dtype)
+        batchv = Variable(batch)
+        batch_size_i = batch.size()[0]
+
+        p_out = get_pred(batchv)
+        preds[i*batch_size:i*batch_size + batch_size_i] = p_out.data.cpu().numpy()
+    
+    # Now compute the kl-div
     py = np.sum(preds, axis=0)
     scores = []
     for i in range(preds.shape[0]):
@@ -61,6 +73,6 @@ def inception_score(imgs, cuda=True, batch_size=32, resize=False):
 
 if __name__ == '__main__':
     print ("Generating images...")
-    imgs = [np.random.uniform(0, 1, size=(64, 64, 3)).astype(np.float16) for _ in range(10)]
+    imgs = [np.random.uniform(0, 1, size=(32, 32, 3)).astype(np.float32) for _ in range(100)]
     print ("Generated Images")
     print (inception_score(imgs, cuda=True, batch_size=32, resize=True))
