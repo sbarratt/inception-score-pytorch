e8952ac38d2248dedb275fec736b2714a785ceb9
Shane Barratt <sbarratt@berkeley.edu>
2017-11-22 10:16:59 -0800

    added splits

--- inception_score.py
+++ inception_score.py
@@ -9,12 +9,13 @@ from torchvision.models.inception import inception_v3
 import numpy as np
 from scipy.stats import entropy
 
-def inception_score(imgs, cuda=True, batch_size=32, resize=False):
+def inception_score(imgs, cuda=True, batch_size=32, resize=False, splits=1):
     """Computes the inception score of the generated images imgs
 
-    imgs -- Torch dataset of (3xHxW) numpy images normalized in the range [0,1]
+    imgs -- Torch dataset of (3xHxW) numpy images normalized in the range [-1, 1]
     cuda -- whether or not to run on GPU
-    batch_size -- batch size to feed into inception
+    batch_size -- batch size for feeding into Inception v3
+    splits -- number of splits
     """
     N = len(imgs)
 
@@ -40,7 +41,7 @@ def inception_score(imgs, cuda=True, batch_size=32, resize=False):
         if resize:
             x = up(x)
         x = inception_model(x)
-        return F.softmax(x)
+        return F.softmax(x).data.cpu().numpy()
 
     # Get predictions
     preds = np.zeros((N, 1000))
@@ -50,32 +51,45 @@ def inception_score(imgs, cuda=True, batch_size=32, resize=False):
         batchv = Variable(batch)
         batch_size_i = batch.size()[0]
 
-        p_out = get_pred(batchv)
-        preds[i*batch_size:i*batch_size + batch_size_i] = p_out.data.cpu().numpy()
+        preds[i*batch_size:i*batch_size + batch_size_i] = get_pred(batchv)
 
     # Now compute the mean kl-div
-    py = np.mean(preds, axis=0)
-    scores = []
-    for i in range(preds.shape[0]):
-        pyx = preds[i, :]
-        scores.append(entropy(pyx, py))
-    mean_kl = np.mean(scores)
+    split_scores = []
 
-    return np.exp(mean_kl)
+    for k in range(splits):
+        part = preds[k * (N // splits): (k+1) * (N // splits), :]
+        py = np.mean(part, axis=0)
+        scores = []
+        for i in range(part.shape[0]):
+            pyx = part[i, :]
+            scores.append(entropy(pyx, py))
+        split_scores.append(np.exp(np.mean(scores)))
+
+    return np.mean(split_scores), np.std(split_scores)
 
 if __name__ == '__main__':
-    print ("Generating images...")
-    class ImageDataset(torch.utils.data.Dataset):
-        def __init__(self, imgs):
-            self.imgs = imgs
+    class IgnoreLabelDataset(torch.utils.data.Dataset):
+        def __init__(self, orig):
+            self.orig = orig
 
         def __getitem__(self, index):
-            return self.imgs[index]
+            return self.orig[index][0]
 
         def __len__(self):
-            return len(self.imgs)
-    imgs = [np.random.uniform(0, 1, size=(3, 32, 32)).astype(np.float32) for _ in range(10)]
-    imgs_dset = ImageDataset(imgs)
+            return len(self.orig)
+
+    import torchvision.datasets as dset
+    import torchvision.transforms as transforms
+
+    cifar = dset.CIFAR10(root='data/', download=True,
+                             transform=transforms.Compose([
+                                 transforms.Scale(32),
+                                 transforms.ToTensor(),
+                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                             ])
+    )
+
+    IgnoreLabelDataset(cifar)
 
     print ("Calculating Inception Score...")
-    print (inception_score(imgs_dset, cuda=True, batch_size=2, resize=True))
+    print (inception_score(IgnoreLabelDataset(cifar), cuda=True, batch_size=32, resize=True, splits=10))
